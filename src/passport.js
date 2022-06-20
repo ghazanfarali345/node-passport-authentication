80221b27533202c02ce603644e90bcf326f19c65
ghazanfarali345 <ghazanfarmalik345@gmail.com>
2022-06-20 13:13:58 +0500

    change callback url

--- src/passport.js
+++ src/passport.js
@@ -33,7 +33,7 @@ passport.use(
     {
       clientID: FACEBOOK_CLIENT_ID,
       clientSecret: FACEBOOK_CLIENT_SECRET,
-      callbackURL: "https://passport-nextjs.vercel.app",
+      callbackURL: "https://passport-nextjs.vercel.app/dashbaord",
       profileFields: [
         "id",
         "email",
